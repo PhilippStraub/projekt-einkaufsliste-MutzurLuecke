1f0a279ee42c432f5bcfecd92705003386ce9f9c
PhilippStraub <54840988+PhilippStraub@users.noreply.github.com>
2019-10-17 15:49:36 +0200

    Add files via upload
    Liste hinzufügen (ohne Request)

---
+++ functions.js
@@ -0,0 +1,8 @@
+function addlist(){
+    var eingabe = prompt("Bitte den Namen der Liste eingeben:", "Listname");
+    if(eingabe != ""){
+        //HTTP Get request
+    } else{
+        alert("Keine Eingabe erhalten!\nBitte erneut versuchen.");
+    }
+}

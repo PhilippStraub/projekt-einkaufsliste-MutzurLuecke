97a8e759fcec42021fd3e0f195ca716165678c5b
PhilippStraub <54840988+PhilippStraub@users.noreply.github.com>
2019-10-18 09:19:00 +0200

    Add files via upload
    Listen werden an der Seite aufgeführt, JS muss noch gemacht werden

--- functions.js
+++ functions.js
@@ -6,3 +6,18 @@ function addlist(){
         alert("Keine Eingabe erhalten!\nBitte erneut versuchen.");
     }
 }
+
+/*
+function add(){
+    var newElement = document.createElement("div");
+    //newElement.textContent = "<h4>Test</h4>";
+	newElement.innerHTML = "<h4>Test</h4>";
+	newElement.className = "class1"
+    document.getElementById("items").appendChild(newElement);
+}
+
+function remove(){
+    document.querySelector("div");
+    document.getElementById("items").parentNode.removeChild("div");
+}
+*/

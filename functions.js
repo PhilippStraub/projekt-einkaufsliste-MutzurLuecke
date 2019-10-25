69d08010465552b3c1b2676fe06de900df2bb20f
Philipp <54840988+PhilippStraub@users.noreply.github.com>
2019-10-25 13:51:46 +0200

    Merge

--- functions.js
+++ functions.js
@@ -1,35 +1,71 @@
 function addlist(){
     var eingabe = prompt("Bitte die ID der Liste eingeben:", "ID");
-    if(eingabe != ""){
-           
-    //Inhalt löschen
-    const e1 = document.getElementById("main");
-    while (e1.firstChild) {
-    e1.removeChild(e1.firstChild);
+    if(eingabe != null){
+        if(eingabe != ""){
+            
+            fetch("https://shopping-lists-api.herokuapp.com/api/v1/lists/"+eingabe).then(
+                function (antwort) {
+                return antwort.json(); 
+                }).then(
+                function (json) {
+                console.log(json["name"]);
+                var nameListe = json["name"];
+                var newElement = document.createElement("div");
+                newElement.innerHTML = nameListe;
+                newElement.className = 'liste';
+                newElement.id = json._id; 
+                newElement.addEventListener('click',
+                function(event) {
+                    console.log(event.target.id);
+                    showlist(event.target.id);
+                });
+                
+
+                
+                
+                //newElement.innerHTML = '<div class="liste" onclick="showlist('+eingabe+')>' + nameListe + '</div>';
+
+            document.getElementById("elemente").appendChild(newElement);
+            showlist(eingabe);
+                
+
+            });
+            
+        } else{
+            alert("Keine Eingabe erhalten!\nBitte erneut versuchen.");
+        }
+    }else{
+        
     }
+}
 
-    //HTTP Get request
-    fetch("https://shopping-lists-api.herokuapp.com/api/v1/lists/5da965bba83b600017fd5c0b").then(
-    function (antwort) {
-    return antwort.json(); 
-    }).then(
-    function (json) {
-    console.log(json["name"]);
-    console.log(json["items"][0]["name"]);
-    });
+function showlist(id){
+    fetch("https://shopping-lists-api.herokuapp.com/api/v1/lists/"+id).then(
+        function(antwort){
+            return antwort.json();
+        }).then(
+            function(json){
+                var c = document.getElementById("mainframe").childNodes;
+                var element = c[0];
+                console.log(element);
 
-    //Neues Element in linke Spalte erstellen      
-    var newElement = document.createElement("div");
-    newElement.innerHTML = '<div onclick="list()">' + eingabe + '</div>';
-    newElement.className = "liste";
-    document.getElementById("elemente").appendChild(newElement);
+                //Elemente davor entfernen
+                const e1 = document.getElementById("mainframe");
+                while (e1.firstChild) {
+                e1.removeChild(e1.firstChild);
+                }
+                for (i = 0; i < Object.keys(json.items).length; i++){
+                    var items = json["items"][i]["name"];
+                    var newElement = document.createElement("div");
+                    newElement.className = "item";
 
-    //Inhalt erstellen
 
+                    newElement.innerHTML = '<label class="switch"><input type="checkbox"><span class="slider round"></span></label>' + items;
+                    document.getElementById("mainframe").appendChild(newElement);
+                }
 
-    } else{
-        alert("Keine Eingabe erhalten!\nBitte erneut versuchen.");
-    }
+            }
+        )
 }
 
 function home(){
@@ -46,7 +82,7 @@ function home(){
     //Neue Elemente anzeigen
     var newElement = document.createElement("div");
 	newElement.innerHTML = '<div class="start" id="begrüßung"><h1>Herzliche willkommen zu deiner Lieblings ToDo-App</h1><h3>Mit diesem lblblblblblblblbllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll</h3></div><div class="start" id="tutorial"><h1 id="tut">Wie funktionert das Ganze?</h1><h3 id="tutu">Nachdem du deine API erhalten hast kannst du auf dem kleinen  Feld links unten eine neue Liste erstellen. Du musst nur den Key in das Textfeld kopieren und schon kannst du in deiner neuen Liste ToDos hinzufügen, als fertig gestellt markieren oder löschen.</h3></div>';
-    newElement.className = "mainframe";
+    newElement.id = "mainframe";
     document.getElementById("main").appendChild(newElement);
 
     var c1 = document.getElementById("main").childNodes;
@@ -64,8 +100,8 @@ function list(){
 
     //Neue Elemente anzeigen
     var newElement = document.createElement("div");
-	newElement.innerHTML = "<div><h4>Test</h4></div>";
-    newElement.className = "mainframe";
+	newElement.innerHTML = '<div class="item">Element1</div><div class="item">Element2</div>';
+    newElement.id = "mainframe";
     document.getElementById("main").appendChild(newElement);
     
 }
